5e49cb11786dd4d50fc3cda88127e97188645562
pipe2442 <felipeangulo2442@gmail.com>
2021-03-22 14:40:02 -0500

    Create posts controller - Create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_193325) do
+ActiveRecord::Schema.define(version: 2021_03_22_193756) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
