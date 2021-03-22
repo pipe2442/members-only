c08f367f5fbe12bd2925c7a019512bc79bf380de
pipe2442 <felipeangulo2442@gmail.com>
2021-03-22 17:47:22 -0500

    Add simple form gem - add post controller methods - add index views with posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,32 @@
 class PostsController < ApplicationController
 
-    before_action :authenticate_user!, except: [:index, :show] 
+    before_action :authenticate_user!, except: [:index, :show]
+    
+    def index
+        @posts = Post.all.order('created_at DESC')
+    end
     
     def new
-        @post = Post.new 
+        @post = current_user.posts.build
+    end
+    
+    def show
     end
 
     def create
+        @post = current_user.posts.build(post_params)
+
+        if @post.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+      
+private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
     end
 
-    def index
-    end
 end
