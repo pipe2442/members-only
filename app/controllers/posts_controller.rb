76f98c5c34758239860cc84b952a0f08a09eec8d
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-23 21:20:49 +0100

    Fix Style/GuardClause linter warning

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,10 +27,14 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content)
   end
 
+  # rubocop:disable Style/GuardClause:
+
   def require_login
     unless user_signed_in?
       flash[:error] = 'You must be logged in to access this section'
       redirect_to new_user_session_path
     end
   end
+
+  # rubocop:enable Style/GuardClause:
 end
