0c8b2a311ecdccb1cc7fd0557bdfa7dee914cfe5
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-23 19:22:04 +0100

    FIx linter errors and warnings

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,29 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
 
-    before_action :authenticate_user!, except: [:index, :show]
-    
-    def index
-        @posts = Post.all.order('created_at DESC')
-    end
-    
-    def new
-        @post = current_user.posts.new
-    end
-    
-    def show
-    end
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
-    def create
-        @post = current_user.posts.new(post_params)
+  def new
+    @post = current_user.posts.new
+  end
 
-        if @post.save
-            redirect_to root_path
-        else
-            render :new
-        end
+  def show; end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
     end
-      
-private
+  end
 
-    def post_params
-        params.require(:post).permit(:title, :content)
-    end
+  private
 
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
